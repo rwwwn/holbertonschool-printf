a363763af74f7b849660eadee35e22e8b0e5af14
rwwwn <ralbaraiki@gmail.com>
2025-04-06 15:36:41 +0000

    Fix: return -1 for single '%' with no specifier (edge case)

--- _printf.c
+++ _printf.c
@@ -1,20 +1,30 @@
 #include "main.h"
+
 /**
  * _printf - custom printf function
  * @format: format string
+ *
  * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
 int i = 0, printed = 0;
 va_list args;
+
 if (!format)
 return (-1);
+
 va_start(args, format);
+
 while (format[i])
 {
-if (format[i] == '%' && format[i + 1])
+if (format[i] == '%')
 {
+if (!format[i + 1])
+{
+va_end(args);
+return (-1);
+}
 i++;
 if (format[i] == 'c')
 printed += handle_char(args);
@@ -29,9 +39,12 @@ printed += _putchar(format[i]);
 }
 }
 else
+{
 printed += _putchar(format[i]);
+}
 i++;
 }
+
 va_end(args);
 return (printed);
 }
