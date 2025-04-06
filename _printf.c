1352557bad9479f1487cd1aad543b60093b73d99
ghadag11221 <10727@holbertonstudents.com>
2025-04-06 14:58:14 +0000

    Task 0: Add _printf, percent, and string implementation

---
+++ _printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _printf - custom printf function
+ * @format: format string
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+int i = 0, printed = 0;
+va_list args;
+if (!format)
+return (-1);
+va_start(args, format);
+while (format[i])
+{
+if (format[i] == '%' && format[i + 1])
+{
+i++;
+if (format[i] == 'c')
+printed += handle_char(args);
+else if (format[i] == 's')
+printed += handle_string(args);
+else if (format[i] == '%')
+printed += handle_percent();
+else
+{
+printed += _putchar('%');
+printed += _putchar(format[i]);
+}
+}
+else
+printed += _putchar(format[i]);
+i++;
+}
+va_end(args);
+return (printed);
+}
---
+++ percent.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * handle_percent - handles %% specifier
+ * Return: number of characters printed
+ */
+int handle_percent(void)
+{
+return (_putchar('%'));
+}
